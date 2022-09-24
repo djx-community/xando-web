65df32fc5493504772c02ffec2cab7ae93f77734
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-09-24 16:45:53 +0530

    ToastContext creation and setup completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import PageOfflineGame from './pages/offline-game/PageOfflineGame';
-import PageUserHome from './pages/user-home/PageUserHome';
-import pages from './constants/pages';
-import PagePlayWithFriend from './pages/play-with-friend/PagePlayWithFriend';
-import PageAboutUs from './pages/about-us/PageAboutUs';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import PageOfflineGame from "./pages/offline-game/PageOfflineGame";
+import PageUserHome from "./pages/user-home/PageUserHome";
+import pages from "./constants/pages";
+import PagePlayWithFriend from "./pages/play-with-friend/PagePlayWithFriend";
+import PageAboutUs from "./pages/about-us/PageAboutUs";
+import { ToastContextProvider } from "./contexts/ToastContext";
 function App() {
   return (
-    <div className="App bg-white dark:bg-gradient-to-r from-slate-900 to-blue-900 dark:text-white h-screen">
-      <Routes>
-        <Route path={pages.HOME} element={<PageUserHome />} />
-        <Route path={pages.PLAY_WITH_FRIEND} element={<PagePlayWithFriend />} />
-        <Route path={pages.OFFLINE_GAME} element={<PageOfflineGame />} />
-        <Route path={pages.ABOUT_US} element={<PageAboutUs />} />
-      </Routes>
-    </div>
+    <ToastContextProvider>
+      <div className="App bg-white dark:bg-gradient-to-r from-slate-900 to-blue-900 dark:text-white h-screen">
+        <Routes>
+          <Route path={pages.HOME} element={<PageUserHome />} />
+          <Route
+            path={pages.PLAY_WITH_FRIEND}
+            element={<PagePlayWithFriend />}
+          />
+          <Route path={pages.OFFLINE_GAME} element={<PageOfflineGame />} />
+          <Route path={pages.ABOUT_US} element={<PageAboutUs />} />
+        </Routes>
+      </div>
+    </ToastContextProvider>
   );
 }
 
---
+++ src/custom-hooks/useToastContext.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import ToastContext from "../contexts/ToastContext";
+
+export default function useToastContext() {
+  return useContext(ToastContext);
+}
+
+/* Using 
+1. import useToastContext from "../../custom-hooks/useToastContext";
+2. const Toast = useToastContext();
+3. const state = {
+    request: true/false,
+    message: "Requested",
+    name: "John doe",
+  };
+  Toast(state);
+  */
