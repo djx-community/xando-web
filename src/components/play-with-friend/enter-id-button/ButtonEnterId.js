f76019950b3e24cac475ef0155103d83e23c5e5c
Abdul Samad M J <samadmj4@gmail.com>
2022-09-24 12:39:40 +0530

    enter id input outline removed

--- src/components/play-with-friend/enter-id-button/ButtonEnterId.js
+++ src/components/play-with-friend/enter-id-button/ButtonEnterId.js
@@ -11,7 +11,8 @@ function ButtonEnterId() {
         shadow-xl shadow-blue-800 h-16 w-56 md:w-30 font-bold p-3
        border-b-4 border-slate-500 hover:border-blue-500 rounded flex gap-1">
             <input type='text' placeholder='Enter ID' value={id} onChange={(e) => setId(e.target.value)}
-                className='text-lg font-medium text-slate-900 dark:text-slate-200 bg-inherit border-b-2 rounded-md h-10 w-40 p-2 text-center' />
+                className="text-lg font-medium text-slate-900 dark:text-slate-200 bg-inherit border-b-2 rounded-md 
+                h-10 w-40 p-2 text-center focus:outline-none" />
             <ChevronRightIcon className='text-white shadow border-2 rounded h-10 bg-inherit hover:bg-sky-700 hover:shadow-white'
                 onClick={() =>
                     handleButtonClick()
