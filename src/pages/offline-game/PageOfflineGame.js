f0c40fdf7ff3c8486c1a0ac366d8f0a9cf9b97c8
Abdul Samad M J <samadmj4@gmail.com>
2022-09-24 20:37:21 +0530

    score board updated

--- src/pages/offline-game/PageOfflineGame.js
+++ src/pages/offline-game/PageOfflineGame.js
@@ -6,10 +6,18 @@ import MatchHeaderArea from '../../components/match-header-area/MatchHeaderArea'
 function PageOfflineGame() {
   const [userScore, setUserScore] = useState(0);
   const [opponentScore, setOpponentScore] = useState(0);
+  const [userProfile, setUserProfile] = useState({
+    username: 'John Doe',
+    userId: '123456789'
+  });
+  const [opponentProfile, setOpponentProfile] = useState({
+    username: 'John Days',
+    userId: '123456789'
+  });
   return (
     <div>
       <AppTitle title={'X & O'} />
-      <MatchHeaderArea scores={{ userScore, opponentScore }} />
+      <MatchHeaderArea userProfile={userProfile} opponentProfile={opponentProfile} scores={{ userScore, opponentScore }} />
       <ForfeitMatchFooter />
     </div>
   )
