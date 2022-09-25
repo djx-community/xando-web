37a5ae1571837aef1c81646a38736ca369c0119e
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-09-25 20:59:41 +0530

    aboutus updation

--- src/components/about-us-card/AboutUSDeveloperCard.jsx
+++ src/components/about-us-card/AboutUSDeveloperCard.jsx
@@ -3,7 +3,7 @@ export default function AboutUsDeveloperCard({ name, icon, url }) {
   return (
     <div className="p-4 flex">
       <h5 className="text-xl w-44">{name}</h5>
-      <a className="pl-5" href={url} target="_blank">
+      <a className="pl-5 hover:scale-110" href={url} target="_blank">
         {icon}
       </a>
     </div>
--- src/components/about-us-card/AboutUsCard.jsx
+++ src/components/about-us-card/AboutUsCard.jsx
@@ -1,9 +1,15 @@
 // General card component for the about us page
-export default function AboutUsCard({ title, icon,url }) {
+export default function AboutUsCard({ title, icon, url }) {
   return (
     <div className="p-5">
       <h5 className="flex  text-xl">{title}</h5>
-      <a className="flex justify-center" href={url} target='_blank'>{icon}</a>
+      <a
+        className="flex justify-center hover:scale-110"
+        href={url}
+        target="_blank"
+      >
+        {icon}
+      </a>
     </div>
   );
 }
