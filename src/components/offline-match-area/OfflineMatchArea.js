5a6cd9890a436b4fa3a16bd634cb2019d7cebc13
Abdul Samad M J <samadmj4@gmail.com>
2022-09-25 13:51:45 +0530

    Basic game design added

--- src/components/offline-match-area/OfflineMatchArea.js
+++ src/components/offline-match-area/OfflineMatchArea.js
@@ -34,10 +34,10 @@ function OfflineMatchArea({ turn, setTurn, setScores }) {
     const checkRows = async () => {
         for (let i = 0; i < 3; i++) {
             if (matrix[i][0].component === matrix[i][1].component && matrix[i][1].component === matrix[i][2].component) {
-                if (matrix[i][0].component === '1') {
-                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: '1' });
-                } else if (matrix[i][0].component === '0') {
-                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: '0' });
+                if (matrix[i][0].component === 'X') {
+                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: 'X' });
+                } else if (matrix[i][0].component === 'O') {
+                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: 'O' });
                 }
             }
         }
@@ -45,10 +45,10 @@ function OfflineMatchArea({ turn, setTurn, setScores }) {
     const checkColumns = async () => {
         for (let i = 0; i < 3; i++) {
             if (matrix[0][i].component === matrix[1][i].component && matrix[1][i].component === matrix[2][i].component) {
-                if (matrix[0][i].component === '1') {
-                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: '1' });
-                } else if (matrix[0][i].component === '0') {
-                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: '0' });
+                if (matrix[0][i].component === 'X') {
+                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: 'X' });
+                } else if (matrix[0][i].component === 'O') {
+                    singleGameWinner({ c1: { row: i, col: 0 }, c2: { row: i, col: 1 }, c3: { row: i, col: 2 }, component: 'O' });
                 }
             }
         }
@@ -56,28 +56,28 @@ function OfflineMatchArea({ turn, setTurn, setScores }) {
 
     const checkDiagonals = async () => {
         if (matrix[0][0].component === matrix[1][1].component && matrix[1][1].component === matrix[2][2].component) {
-            if (matrix[0][0].component === '1') {
-                singleGameWinner({ c1: { row: 0, col: 0 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 2 }, component: '1' });
-            } else if (matrix[0][0].component === '0') {
-                singleGameWinner({ c1: { row: 0, col: 0 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 2 }, component: '0' });
+            if (matrix[0][0].component === 'X') {
+                singleGameWinner({ c1: { row: 0, col: 0 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 2 }, component: 'X' });
+            } else if (matrix[0][0].component === 'O') {
+                singleGameWinner({ c1: { row: 0, col: 0 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 2 }, component: 'O' });
             }
         } else if (matrix[0][2].component === matrix[1][1].component && matrix[1][1].component === matrix[2][0].component) {
-            if (matrix[1][1].component === '1') {
-                singleGameWinner({ c1: { row: 0, col: 2 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 0 }, component: '1' });
-            } else if (matrix[1][1].component === '0') {
-                singleGameWinner({ c1: { row: 0, col: 2 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 0 }, component: '0' });
+            if (matrix[1][1].component === 'X') {
+                singleGameWinner({ c1: { row: 0, col: 2 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 0 }, component: 'X' });
+            } else if (matrix[1][1].component === 'O') {
+                singleGameWinner({ c1: { row: 0, col: 2 }, c2: { row: 1, col: 1 }, c3: { row: 2, col: 0 }, component: 'O' });
             }
         }
     }
 
     const singleGameWinner = async ({ c1, c2, c3, component }) => {
         //doing animation
-        if (component === '1') {
+        if (component === 'X') {
             alert('X wins');
             setScores((prev) => {
                 return { ...prev, userScore: prev.userScore + 1 }
             });
-        } else if (component === '0') {
+        } else if (component === 'O') {
             alert('O wins');
             setScores((prev) => {
                 return { ...prev, opponentScore: prev.opponentScore + 1 }
@@ -109,20 +109,24 @@ function OfflineMatchArea({ turn, setTurn, setScores }) {
             <div className="">
                 {
                     matrix.map((row, rowIndex) => {
-                        return (<div key={rowIndex} className='flex gap-1'>
+                        return (<div key={rowIndex} className='flex'>
                             {
                                 row.map((cell, cellIndex) => {
-                                    return (<button key={rowIndex + 'x' + cellIndex} className='bg-gray-900'
+                                    return (<button key={rowIndex + 'x' + cellIndex}
+                                        className={`m-1 w-20 h-20 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md 
+                                       ${!cell.clicked && 'hover:bg-gray-100 dark:hover:bg-gray-700'}  dark:bg-gray-800 dark:border-gray-700 text-xl`}
                                         disabled={cell.clicked}
                                         onClick={() => {
                                             setMatrix((prevMatrix) => {
                                                 const newMatrix = [...prevMatrix];
-                                                newMatrix[rowIndex][cellIndex] = (turn ? { component: '1', clicked: true } : { component: '0', clicked: true });
+                                                newMatrix[rowIndex][cellIndex] = (turn ? { component: 'X', clicked: true } : { component: 'O', clicked: true });
                                                 return newMatrix;
                                             });
                                             setTurn((prevTurn) => !prevTurn);
                                             setCountTurn(countTurn + 1);
-                                        }}>{cell.component ? cell.component : 'tile ' + rowIndex + 'x' + cellIndex}&nbsp;</button>)
+                                        }}>{cell.component ? cell.component : ' '}&nbsp;
+
+                                    </button>)
                                 })
                             }
                             <br />
