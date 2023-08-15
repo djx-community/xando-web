efce874a09012d43a73116dd6b2abf5a2f02bcb6
Abdul Samad M J <samadmj4@gmail.com>
2023-08-15 13:05:47 +0530

    re-render issue fixed

--- src/components/turn-indicator/TurnIndicator.jsx
+++ src/components/turn-indicator/TurnIndicator.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function TurnIndicator ({ turn, opponent }) {
   const [turnState, setTurnState] = React.useState(turn)
-  if (opponent) {
-    setTurnState(!turnState)
+  React.useEffect(() => {
+    if (opponent) {
+      setTurnState(!turnState)
     // var turn = !turn
-  }
+    }
+  }, [turn, opponent])
   return (
         <div className={`absolute top-40 ${opponent ? 'md:left-36 left-4' : 'md:right-36 right-4'}`}>
             <h3 className={`md:text-2xl text-lg border-2 border-white rounded-full bg-slate-800 p-5
